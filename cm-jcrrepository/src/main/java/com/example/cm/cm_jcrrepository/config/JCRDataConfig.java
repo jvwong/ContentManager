1fc2cc64eeee30759cb238216b61f27c492b27a5
Jeffrey V Wong <jvwong@outlook.com>
2015-10-19 13:34:28 -0400

    jcr data config name change

---
+++ cm-jcrrepository/src/main/java/com/example/cm/cm_jcrrepository/config/JCRDataConfig.java
@@ -0,0 +1,36 @@
+package com.example.cm.cm_jcrrepository.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+
+import javax.jcr.SimpleCredentials;
+
+
+@Configuration
+//@ComponentScan(
+//	basePackages="com.example.cm.cm_jcrrepository.jcr"
+//)
+//@PropertySource("classpath:/com/example/cm/cm_jcrrepository/environment.properties")
+@ImportResource("classpath:beans-jcr.xml")
+public class JCRDataConfig {
+
+//	@Autowired
+//	Environment env;
+//
+//	@Bean
+//	public SimpleCredentials credentials(){
+//		String username = env.getProperty("username");
+//		String password = env.getProperty("password");
+//		SimpleCredentials credentials
+//				= new SimpleCredentials(username, password.toCharArray());
+//		return credentials;
+//	}
+//
+////	@Bean
+////	RepositoryFactoryBean repository(){
+////		RepositoryFactoryBean repositoryFactoryBean
+////				= new RepositoryFactoryBean();
+////	}
+
+}
